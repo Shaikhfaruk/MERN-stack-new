749e36fce086b92aef08cfa8fa9dc0d493ef5f84
Shaikh Faruk <sfaruk1137@gmail.com>
2021-07-07 13:42:49 +0530

    okk

--- server/app.js
+++ server/app.js
@@ -1,10 +1,34 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+// create a data base... when you get a link from mongodb you need to change myNewProject name and password
+const DB =
+  "mongodb+srv://faruk:farukshaikh@cluster0.8zamo.mongodb.net/mern-learn?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("connection successfully");
+  })
+  .catch((err) => console.log("connection error"));
+
+// Middelware for secure your useNewConnection
+
+const middleware = (req, res, next) => {
+  console.log("middleware okk");
+  next();
+};
 
 app.get("/", (req, res) => {
   res.send(`Hello world, I'm home page`);
 });
-app.get("/about", (req, res) => {
+app.get("/about", middleware, (req, res) => {
   res.send(`Hello world, I'm about page`);
 });
 app.get("/contact", (req, res) => {
