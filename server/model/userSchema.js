cf5c3a9bc094a9b5247c28f11301527313d57327
Shaikh Faruk <sfaruk1137@gmail.com>
2021-07-14 12:40:15 +0530

    probel

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const User = require("../model/userSchema");
 
 const userSchema = new mongoose.Schema({
   name: {
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+require("../db/conn");
+const User = require("../model/userSchema");
+
 router.get("/", (req, res) => {
   res.send(`Hello world, I'm home page from router js`);
 });
@@ -8,9 +11,31 @@ router.get("/", (req, res) => {
 router.post("/register", (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
 
-  console.log(req.body);
-  console.log(name);
-  console.log(email);
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "Invalid username or email" });
+  }
+
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ error: "Email already exists" });
+      }
+      const user = new User({ name, email, phone, work, password, cpassword });
+
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: "User saved successfully" });
+        })
+        .catch((err) => res.status(500).json({ error: "Failed to save user" }));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // console.log(req.body);
+  // console.log(name);
+  // console.log(email);
   // res.json({ message: req.body });
   // res.send("mera registered ho rha hai");
 });
