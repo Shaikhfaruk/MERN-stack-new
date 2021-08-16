7d9cb1bf75563d27fc0bb46d78651ede466763d5
[Shaikh Faruk] <[sfaruk1137@gmail.com]>
2021-08-16 07:22:01 -0700

    Using Grid Syst fix Contact Page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,9 +1,43 @@
 import React from "react";
+import "../styles/contact.css";
+import phone from "../images/phone.png";
+import email from "../images/email.png";
+import placeholder from "../images/placeholder.png";
 
 const Contact = () => {
   return (
     <>
-      <h1>contact us</h1>
+      <section className="contact_info">
+        <div className="container-fluid mt-5">
+          <div className="row ">
+            <div className="contact-div col-lg-10 offset-lg-1">
+              <div className="contact_info_item">
+                <img src={phone} alt="phone" />
+                <div className="contact_info_content">
+                  <div className="contact_info_title">Phone</div>
+                  <div className="contact_info_text">+91 966661828</div>
+                </div>
+              </div>
+              <div className="contact_info_item">
+                <img src={email} alt="email" />
+                <div className="contact_info_content">
+                  <div className="contact_info_title">Email</div>
+                  <div className="contact_info_text">sfaruk1137@gmail.com</div>
+                </div>
+              </div>
+              <div className="contact_info_item">
+                <img src={placeholder} alt="placeholder" />
+                <div className="contact_info_content">
+                  <div className="contact_info_title">Address</div>
+                  <div className="contact_info_text">
+                    Beed, Maharashtra, India{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import login from "../images/Login1.svg";
 
 const Login = () => {
   return (
     <>
-      <p>Welcome</p>
-      <h1>Login</h1>
+      <section className="signup">
+        <div className="container mt-5">
+          <div className="signup-image">
+            <figure>
+              <img src={login} alt="loginPic" />
+            </figure>
+            <Link to="/signup" className="signup-image-link">
+              Create An Account?
+            </Link>
+          </div>
+          <div className="signup-form">
+            <h2 className="form-title">Login</h2>
+            <form className="register-form" id="register-form">
+              <div className="form-group">
+                <label htmlFor="email">
+                  <i className="zmdi zmdi-email material-icons-name"></i>
+                </label>
+                <input
+                  className="effect"
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="off"
+                  placeholder="Your Email"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password">
+                  <i className="zmdi zmdi-lock material-icons-name"></i>
+                </label>
+                <input
+                  className="effect"
+                  type="password"
+                  name="password"
+                  id="password"
+                  autoComplete="off"
+                  placeholder="Your Password"
+                />
+              </div>
+
+              <div className=" form-button">
+                <input
+                  type="submit"
+                  name="signup"
+                  id="signup"
+                  className="form-submit"
+                  value="log in"
+                />
+              </div>
+              <Link to="/login" className="signup-mobile-link">
+                Already have an account ?
+              </Link>
+            </form>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
